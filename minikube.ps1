9bc38ac1d0346bbf0d7d09347dc5bfe1eaa8fc31
mangila <olsson.erik1993@gmail.com>
2025-02-17 23:02:11 +0100

    wrapped mvn, k8s

--- minikube.ps1
+++ minikube.ps1
@@ -1,3 +1,13 @@
-# k8s
-
-kubectl get deployments
+# Maven - clean and package
+cmd.exe /c mvnw.cmd clean package
+# Docker - build
+docker build -f ./scheduler/Dockerfile -t mangila/pokedex-scheduler:latest ./scheduler
+docker build -f ./graphql-server/Dockerfile -t mangila/pokedex-graphql-server:latest ./graphql-server
+docker build -f ./file-server/Dockerfile -t mangila/pokedex-file-server:latest ./file-server
+# K8s apply
+kubectl -- apply -f config-k8s.yml
+kubectl -- apply -f secret-k8s.yml
+kubectl -- apply -f service-k8s.yml
+kubectl -- apply -f /graphql-server/k8s.yml
+kubectl -- apply -f /file-server/k8s.yml
+kubectl -- apply -f /scheduler/k8s.yml
