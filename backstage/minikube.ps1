bffd5723d4f6e29a15fd2e9843916460457a5930
mangila <olsson.erik1993@gmail.com>
2025-03-12 16:17:06 +0100

    pollishing

--- backstage/minikube.ps1
+++ backstage/minikube.ps1
@@ -1,26 +1,55 @@
-# Image-Converter
-$imageConverterDir = "./image-converter"
-docker build -f ${imageConverterDir}/Dockerfile -t mangila/pokedex-image-converter:latest ${imageConverterDir}
-# Build images - start build context from root, since we need the shared modules compiled
-# Tasks
-$generationTaskDir = "./generation-task"
-$pokemonTaskDir = "./pokemon-task"
-$mediaTaskDir = "./media-task"
-docker build -f ${generationTaskDir}/Dockerfile -t mangila/pokedex-generation-task:latest .
-docker build -f ${pokemonTaskDir}/Dockerfile -t mangila/pokedex-pokemon-task:latest .
-docker build -f ${mediaTaskDir}/Dockerfile -t mangila/pokedex-media-task:latest .
-# Bouncer
-$pokeApiBouncer = "./pokeapi-bouncer"
-$redisBouncerkDir = "./redis-bouncer"
-$mongoDbBouncerkDir = "./mongodb-bouncer"
-docker build -f ${pokeApiBouncer}/Dockerfile -t mangila/pokedex-pokeapi-bouncer:latest .
-docker build -f ${redisBouncerkDir}/Dockerfile -t mangila/pokedex-redis-bouncer:latest .
-docker build -f ${mongoDbBouncerkDir}/Dockerfile -t mangila/pokedex-mongodb-bouncer:latest .
-# K8s - deploy specs
-kubectl -- apply -f ${imageConverterDir}/k8s.yml
-kubectl -- apply -f ${generationTaskDir}/k8s.yml
-kubectl -- apply -f ${pokemonTaskDir}/k8s.yml
-kubectl -- apply -f ${mediaTaskDir}/k8s.yml
-kubectl -- apply -f ${pokeApiBouncer}/k8s.yml
-kubectl -- apply -f ${redisBouncerkDir}/k8s.yml
-kubectl -- apply -f ${mongoDbBouncerkDir}/k8s.yml
+# Image-Converter : START
+$dir = "./image-converter"
+$dockerTag = "mangila/pokedex-image-converter:latest";
+docker build -f ${dir}/Dockerfile -t ${dockerTag} ${dir}
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Image-Converter : END
+
+# Pokeapi-Bouncer : START
+$dir = "./pokeapi-bouncer"
+$dockerTag = "mangila/pokedex-pokeapi-bouncer:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Pokeapi-Bouncer : END
+
+# Redis-Bouncer : START
+$dir = "./redis-bouncer"
+$dockerTag = "mangila/pokedex-redis-bouncer:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Redis-Bouncer : END
+
+# Mongodb-Bouncer : START
+$dir = "./mongodb-bouncer"
+$dockerTag = "mangila/pokedex-mongodb-bouncer:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Mongodb-Bouncer : END
+
+# Generation-Task : START
+$dir = "./generation-task"
+$dockerTag = "mangila/pokedex-generation-task:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Generation-Task : END
+
+# Pokemon-Task : START
+$dir = "./pokemon-task"
+$dockerTag = "mangila/pokedex-pokemon-task:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Pokemon-Task : END
+
+# Media-Task : START
+$dir = "./media-task"
+$dockerTag = "mangila/pokedex-media-task:latest"
+docker build -f ${dir}/Dockerfile -t ${dockerTag} .
+minikube image load ${dockerTag}
+#kubectl --apply -f ${dir}/k8s.yml
+# Media-Task : END
