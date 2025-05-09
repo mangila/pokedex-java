d61ffa23f1ba9d4c308b0017958d2a59d6d3e978
mangila <olsson.erik1993@gmail.com>
2025-05-09 18:55:35 +0200

    connection pool

--- pokedex/shared/src/main/java/com/github/mangila/pokedex/shared/config/HttpConfig.java
+++
@@ -1,4 +0,0 @@
-package com.github.mangila.pokedex.shared.config;
-
-public record HttpConfig() {
-}
--- pokedex/shared/src/main/java/com/github/mangila/pokedex/shared/func/VoidFunction.java
+++
@@ -1,6 +0,0 @@
-package com.github.mangila.pokedex.shared.func;
-
-@FunctionalInterface
-public interface VoidFunction {
-    void apply();
-}
