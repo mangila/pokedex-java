6f79ef27ea5a81ef383080d3bdf5d1854e3f6dab
mangila <olsson.erik1993@gmail.com>
2025-05-18 15:29:26 +0200

    github

---
+++ pokedex/shared/src/main/java/com/github/mangila/pokedex/shared/model/PokeApiUriGithub.java
@@ -0,0 +1,29 @@
+package com.github.mangila.pokedex.shared.model;
+
+import java.net.URI;
+import java.util.Objects;
+
+public record PokeApiUriGithub(URI uri) {
+
+    public PokeApiUriGithub {
+        Objects.requireNonNull(uri);
+        ensureFromPokeApi(uri);
+    }
+
+    public static PokeApiUriGithub fromString(String uri) {
+        return new PokeApiUriGithub(URI.create(uri));
+    }
+
+    public String getPath() {
+        return uri.getPath();
+    }
+
+    private void ensureFromPokeApi(URI uri) {
+        if (!uri.getHost().equals("raw.githubusercontent.com")) {
+            throw new IllegalArgumentException("Host name must be 'pokeapi.co' - " + uri.getHost());
+        }
+        if (!uri.getPath().startsWith("/PokeApi")) {
+            throw new IllegalArgumentException("Path must start with '/api/v2' - " + uri.getPath());
+        }
+    }
+}
