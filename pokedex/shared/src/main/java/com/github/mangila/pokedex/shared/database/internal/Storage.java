32de3828e964aa760dcfa33e407905b646ec4250
mangila <olsson.erik1993@gmail.com>
2025-05-26 23:25:58 +0200

    db

--- pokedex/shared/src/main/java/com/github/mangila/pokedex/shared/database/internal/Storage.java
+++ pokedex/shared/src/main/java/com/github/mangila/pokedex/shared/database/internal/Storage.java
@@ -1,17 +1,25 @@
 package com.github.mangila.pokedex.shared.database.internal;
 
 import com.github.mangila.pokedex.shared.model.Pokemon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Storage {
 
+    private static final Logger log = LoggerFactory.getLogger(Storage.class);
     private final String fileName;
 
     public Storage(String fileName) {
         try {
-            new File(fileName).createNewFile();
+            var exist = new File(fileName).createNewFile();
+            if (!exist) {
+                log.info("File {} already exists", fileName);
+            } else {
+                log.info("File {} created", fileName);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
